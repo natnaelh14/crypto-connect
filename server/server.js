750f548e92a8da74391262c436608b0d2e534fc1
natnaelh14 <natnaelh14@github.com>
2021-12-06 08:52:50 -0500

    connnected cliend and server

--- server/server.js
+++ server/server.js
@@ -43,9 +43,9 @@ async function startApolloServer() {
   //     res.sendFile(index);
   //  });
 
-    // app.get('*', (req, res) => {
-    //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    // });
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
     
     await server.start();
     server.applyMiddleware({ app });
