bbd18abe9bcef91aa014671732d3b3a9be61587a
natnaelh14 <natnaelh14@github.com>
2021-12-12 11:14:44 -0500

    enabled github and twitter authentication

--- client/src/components/AboutUs/about-us.component.tsx
+++ client/src/components/AboutUs/about-us.component.tsx
@@ -4,10 +4,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import feature from './feature.jpg';
 import { ImageContainer } from './about-us.styles';
+import './about.css';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        marginBottom: '200px',
+        marginBottom: '100px',
         marginTop: '3rem',
         display: "flex",
         justifyContent: "center"
@@ -15,15 +16,45 @@ const useStyles = makeStyles((theme) => ({
     paper: {
         margin: theme.spacing(2, 6),
     },
+    featureTitle: {
+        fontSize: '3rem !Important',
+        fontWeight: 900,
+        color: '#222060',
+        marginBottom: '1rem',
+        fontFamily: 'inherit',
+        textAlign: 'center',
+        "@media (max-width: 800px)": {
+            fontSize: '1.9rem !Important'
+        }
+    },
+    featureText: {
+        lineHeight: 2,
+        textIndent: '-1.4em', 
+        padding: '10px',
+        fontFamily: 'inherit',
+        textAlign: 'left',
+        "&::before": {
+            content: '▹',
+            paddingRight: '1em',
+            color: 'red'
+        },
+        "@media (max-width: 800px)": {
+            fontSize: '0.8rem'
+        }
+    },
+    featureContainer: {
+        "@media (max-width: 1200px)": {
+            marginBottom: '6rem'
+        }
+    },
 }))
 
 const AboutUsPage = () => {
     const classes = useStyles();
     return (
-        <section style={{ marginTop: '500px' }}>
-            <Typography sx={{ fontSize: '3rem !Important', fontWeight: 800 }} mb='2rem' fontFamily='inherit' textAlign='center'>FEATURES</Typography>
+        <section style={{ marginTop: '400px' }}>
             <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: "space-around" }}>
-                <Box>
+                <Box className={classes.featureContainer}>
                     <Grid container component="main" className={classes.root}>
                         <Box
                             sx={{ margin: '20px', width: '500px' }}
@@ -35,14 +66,15 @@ const AboutUsPage = () => {
                                 square
                                 sx={{ margin: '20px', padding: '20px', width: '500px', display: "flex", flexDirection: "column" }}
                             >
-                                <Typography fontFamily='inherit' textAlign='left' sx={{ padding: '10px' }} >▹ USER AUTHENTICATION USING GOOGLE</Typography>
-                                <Typography fontFamily='inherit' textAlign='left' sx={{ padding: '10px' }} >▹ PERSONALIZED USER PROFILE</Typography>
-                                <Typography fontFamily='inherit' textAlign='left' sx={{ padding: '10px' }} >▹ POST / COMMENT / LIKES / DISLIKES</Typography>
-                                <Typography fontFamily='inherit' textAlign='left' sx={{ padding: '10px' }} >▹ SHARE POSTS AND TWEETS</Typography>
-                                <Typography fontFamily='inherit' textAlign='left' sx={{ padding: '10px' }} >▹ SEND MESSAGES</Typography>
-                                <Typography fontFamily='inherit' textAlign='left' sx={{ padding: '10px' }} >▹ FOLLOW USERS</Typography>
-                                <Typography fontFamily='inherit' textAlign='left' sx={{ padding: '10px', lineHeight: '2rem' }} >▹ CREATE PERSONALIZED CRYPTOCURRENCY PORTFOLIO</Typography>
-                                <Typography fontFamily='inherit' textAlign='left' sx={{ padding: '10px' }} >▹ REAL-TIME CRYPTO PRICING, TWEETS ABOUT CRYPTO HOLDING AND OTHER CRYPTO INFO</Typography>
+                                <Typography className={classes.featureTitle}>FEATURES</Typography>
+                                <Typography id='bulletin' className={classes.featureText}>USER AUTHENTICATION USING GOOGLE, GITHUB, TWITTER</Typography>
+                                <Typography id='bulletin' className={classes.featureText}>PERSONALIZED USER PROFILE</Typography>
+                                <Typography id='bulletin' className={classes.featureText}>POSTS / COMMENTS / LIKES / DISLIKES</Typography>
+                                <Typography id='bulletin' className={classes.featureText}>SHARE POSTS AND TWEETS</Typography>
+                                <Typography id='bulletin' className={classes.featureText}>SEND MESSAGES</Typography>
+                                <Typography id='bulletin' className={classes.featureText}>FOLLOW USERS</Typography>
+                                <Typography id='bulletin' className={classes.featureText}>CREATE PERSONALIZED CRYPTOCURRENCY PORTFOLIO</Typography>
+                                <Typography id='bulletin' className={classes.featureText}>REAL-TIME CRYPTO PRICING, TWEETS ABOUT CRYPTO HOLDING AND OTHER CRYPTO INFO</Typography>
                             </Grid>
                         </Box>
                     </Grid>
@@ -52,10 +84,6 @@ const AboutUsPage = () => {
                 </ImageContainer>
             </Box>
         </section>
-
-
-
-
     )
 }
 
